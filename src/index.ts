0271c812639380644c4cba0906d59eca0a22c746
gorden <limguancheng@hotmail.com>
2022-05-23 18:07:01 +0800

    install prisma

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import express, { Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import cors from 'cors';
-import { Pool } from 'pg';
+// import { Pool } from 'pg';
 import 'dotenv/config'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 const NODE_ENV = process.env.NODE_ENV || 'production';
-const pool = new Pool({
-  user: 'postgres',
-  host: NODE_ENV === 'production' ? 'host.docker.internal' : '192.168.0.8',
-  database: 'ionicerp',
-  password: 'helloworld',
-  port: 5432,
-});
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: NODE_ENV === 'production' ? 'host.docker.internal' : '192.168.0.8',
+//   database: 'ionicerp',
+//   password: 'helloworld',
+//   port: 5432,
+// });
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -25,36 +27,42 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server is running...');
 });
 
-app.get('/users', (req: Request, res: Response) => {
-  pool.query('SELECT * FROM accounts', (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(200).json(results.rows)
+app.get('/:entity', async (req: Request, res: Response) => {
+  // Creating a new record
+  // const entity: string = req.params.entity
+  const a = await prisma.accounts.findMany({
+    select: {
+      username: true,
+    },
   });
+  console.log(a);
+
+  // const table = req.params.entity;
+
+  res.status(200).json({})
 });
 
-app.post('/users', (req: Request, res: Response) => {
-  const { name } = req.body;
-  const uuid = uuidv4();
+// app.post('/users', (req: Request, res: Response) => {
+//   const { name } = req.body;
+//   const uuid = uuidv4();
 
-  pool.query('INSERT INTO users (uuid, name) VALUES ($1, $2)', [uuid, name], (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(201).send({ result: 'User added!' })
-  });
-});
+//   pool.query('INSERT INTO users (uuid, name) VALUES ($1, $2)', [uuid, name], (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     res.status(201).send({ result: 'User added!' })
+//   });
+// });
 
-app.delete('/users/:id', (req: Request, res: Response) => {
-  const id = req.params.id;
-  pool.query('DELETE FROM users WHERE uuid = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(200).send({ result: `User deleted with ID: ${id}` })
-  });
-});
+// app.delete('/users/:id', (req: Request, res: Response) => {
+//   const id = req.params.id;
+//   pool.query('DELETE FROM users WHERE uuid = $1', [id], (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     res.status(200).send({ result: `User deleted with ID: ${id}` })
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
