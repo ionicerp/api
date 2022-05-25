aed0940066ff50ee7228d0bce88ec8975d8da579
gorden <limguancheng@hotmail.com>
2022-05-25 18:12:53 +0800

    catch error

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,6 @@ const correspondingDatabaseTable = (tableName: string) => {
   return EntityMap.get(tableName)!;
 };
 
-
 /**
  * Applied SOLID principle: Open to scale this API, closed to modification this API.
  * Every new table should be added to the EntityMap, with the corresponding table class.
@@ -52,15 +51,16 @@ app.put('/:entity', authenticateToken, async (req: Request, res: Response) => {
   const entity: string = req.params.entity;
   const databaseTable: DatabaseTable = correspondingDatabaseTable(entity);
   if (!databaseTable) return res.status(400).send({ result: 'entity not found!' });
-  databaseTable.create(req.body).then(result => {
-    return res.status(200).send({ status: 'success', data: result, message: 'record created' });
-  }).catch(error => {
-    return res.status(400).send({ status: 'error', data: error, message: 'record not created' });
-  });
+  try {
+    const result = await databaseTable.create(req.body);
+    return res.status(200).send({ status: 'success', data: result, message: 'Record created' });
+  } catch (e: any) {
+    return res.status(500).send({ status: 'error', message: e.message });
+  }
 });
 
 
-app.get('/:entity', async (req: Request, res: Response) => {
+app.get('/:entity', authenticateToken, async (req: Request, res: Response) => {
   const entity: string = req.params.entity;
   const page: number = parseInt(req.query.page as string) ?? 1;
   const limit: number = req.query.limit ? parseInt(req.query.limit as string) : 10;
@@ -68,39 +68,44 @@ app.get('/:entity', async (req: Request, res: Response) => {
 
   const databaseTable: DatabaseTable = correspondingDatabaseTable(entity);
   if (!databaseTable) return res.status(400).send({ result: 'entity not found!' });
-  const result = await databaseTable.list(condition, limit);
-
-  res.status(200).send({ status: 'success', data: result, message: 'record created' });
-
+  try {
+    const result = await databaseTable.list(condition, limit);
+    return res.status(200).send({ status: 'success', data: result, message: `${result.length ?? 0} ${entity} get` });
+  } catch (e: any) {
+    return res.status(500).send({ status: 'error', message: e.message });
+  }
 });
 
-
 // get single record
-app.get('/:entity/:id', async (req: Request, res: Response) => {
+app.get('/:entity/:id', authenticateToken, async (req: Request, res: Response) => {
   const entity: string = req.params.entity;
+  const id: number = parseInt(req.params.id);
   const databaseTable: DatabaseTable = correspondingDatabaseTable(entity);
   if (!databaseTable) return res.status(400).send({ result: 'entity not found!' });
-  const id: number = parseInt(req.params.id);
-  const result = await databaseTable.get(id);
-  return res.status(200).send({ status: 'success', data: result, message: 'record found' });
+  try {
+    const result = await databaseTable.get(id);
+    return res.status(200).send({ status: 'success', data: result, message: 'success' });
+  } catch (e: any) {
+    return res.status(500).send({ status: 'error', message: e.message });
+  }
 })
 
 // update single record
-app.post('/:entity', async (req: Request, res: Response) => {
+app.post('/:entity', authenticateToken, async (req: Request, res: Response) => {
   const entity: string = req.params.entity;
   const databaseTable: DatabaseTable = correspondingDatabaseTable(entity);
   if (!databaseTable) return res.status(400).send({ result: 'entity not found!' });
-
+  // const result = await databaseTable.update(req.body);
   res.status(200).send({ result: 'ok' });
 })
 
 // delete single record
-app.delete('/:entity', async (req: Request, res: Response) => {
+app.delete('/:entity', authenticateToken, async (req: Request, res: Response) => {
   const entity: string = req.params.entity;
   const databaseTable: DatabaseTable = correspondingDatabaseTable(entity);
   if (!databaseTable) return res.status(400).send({ result: 'entity not found!' });
 
-  res.status(200).send({ result: 'ok' });
+  res.status(202).send({ result: 'ok' });
 })
 
 
